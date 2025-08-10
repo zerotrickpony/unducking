5eac5b52403cc18ff968517a44688c8da16c69d9
rus <rus@zerotrickpony.com>
2025-08-10 12:59:03 -0700

    remove broken charts

--- web/results.js
+++ web/results.js
@@ -2,12 +2,9 @@ function main() {
   const envs = new Set(ALL_COMPARISONS.map(r => r.environment));
 
   for (const env of envs) {
-    //addCompareChart(env, ALL_COMPARISONS.filter(r => r.environment === env));
-    addDataChart(env, ALL_RESULTS.filter(r => r.environment === env));
+    addDataTable(env, ALL_RESULTS.filter(r => r.environment === env));
   }
 }
-
-let CHART_NUMBER = 1;
 
 function addNode(flavor, id) {
   const node = document.createElement(flavor);
@@ -18,86 +15,8 @@ function addNode(flavor, id) {
   return node;
 }
 
-function addCompareChart(env, data) {
-  // Header and chart holder
+function addDataTable(env, data) {
   addNode('h1').innerText = `Environment: ${env}`;
-
-  const chartId = `chart${CHART_NUMBER++}`;
-  const div = addNode('div', chartId);
-  div.setAttribute('style', 'height: 300px;');
-
-  // Draw
-  const root = am5.Root.new(chartId);
-  const chart = root.container.children.push(
-    am5xy.XYChart.new(root, {
-      layout: root.verticalLayout
-    })
-  );
-
-  // Make categories out of the four comparison flavors
-  const xRenderer = am5xy.AxisRendererX.new(root, {
-  cellStartLocation: 0.1,
-  cellEndLocation: 0.9,
-  minorGridEnabled: true
-  });
-  const xAxis = chart.xAxes.push(
-    am5xy.CategoryAxis.new(root, {
-      renderer: xRenderer,
-      categoryField: "comparetest"
-    })
-  );
-  xRenderer.grid.template.setAll({
-    location: 1
-  })
-  const comparetests = [...new Set(data.map(r => r.comparetest))];
-  xAxis.data.setAll(comparetests.map(comparetest => {return {comparetest};}));
-
-  const legend = chart.children.push(
-    am5.Legend.new(root, {
-      centerX: am5.p50,
-      x: am5.p50
-    })
-  );
-
-  const yAxis = chart.yAxes.push(
-    am5xy.ValueAxis.new(root, {
-      min: 0,
-      renderer: am5xy.AxisRendererY.new(root, {
-        strokeOpacity: 1,
-      })
-    })
-  );
-
-  const series = chart.series.push(
-    am5xy.ColumnSeries.new(root, {
-      name: env,
-      xAxis: xAxis,
-      yAxis: yAxis,
-      valueYField: "testRatio",
-      categoryXField: "comparetest",
-      opacity: 0
-    })
-  );
-  series.bullets.push(() => {
-    const graphics = am5.Circle.new(root, {
-      fill: series.get("fill"),
-      radius: 2
-    });
-    return am5.Bullet.new(root, {
-      sprite: graphics
-    });
-  });
-
-  series.data.setAll(data);
-}
-
-function addDataChart(env, data) {
-  // Header and chart holder
-  addNode('h1').innerText = `Environment: ${env}`;
-
-  const chartId = `chart${CHART_NUMBER++}`;
-  const div = addNode('div', chartId);
-  div.setAttribute('style', 'height: 300px;');
 
   // Summary stats table
   const summary = addNode('div');
@@ -121,72 +40,6 @@ function addDataChart(env, data) {
     </tr>`;
   }
   summary.innerHTML = html + '</table>';
-
-  // Draw
-  const root = am5.Root.new(chartId);
-  const chart = root.container.children.push(
-    am5xy.XYChart.new(root, {
-      layout: root.verticalLayout
-    })
-  );
-
-  // Make categories out of the four comparison flavors
-  const xRenderer = am5xy.AxisRendererX.new(root, {
-  cellStartLocation: 0.1,
-  cellEndLocation: 0.9,
-  minorGridEnabled: true
-  });
-  const xAxis = chart.xAxes.push(
-    am5xy.CategoryAxis.new(root, {
-      renderer: xRenderer,
-      categoryField: "testname"
-    })
-  );
-  xRenderer.grid.template.setAll({
-    location: 1
-  })
-  const testnames = [...new Set(data.map(r => r.testname))];
-  xAxis.data.setAll(testnames.map(testname => {return {testname};}));
-
-  const legend = chart.children.push(
-    am5.Legend.new(root, {
-      centerX: am5.p50,
-      x: am5.p50
-    })
-  );
-
-  const yAxis = chart.yAxes.push(
-    am5xy.ValueAxis.new(root, {
-      renderer: am5xy.AxisRendererY.new(root, {
-        minorGridEnabled: true,
-        strokeOpacity: 1,
-      })
-    })
-  );
-
-  const series = chart.series.push(
-    am5xy.ColumnSeries.new(root, {
-      name: env,
-      xAxis: xAxis,
-      yAxis: yAxis,
-      valueYField: "durationMs",
-      categoryXField: "testname",
-      opacity: 0
-    })
-  );
-  series.bullets.push(() => {
-    const graphics = am5.Circle.new(root, {
-      fill: series.get("fill"),
-      radius: 2
-    });
-    return am5.Bullet.new(root, {
-      sprite: graphics,
-      tooltip: am5.Tooltip.new(root, {})
-
-    });
-  });
-
-  series.data.setAll(data);
 }
 
 document.addEventListener('DOMContentLoaded', () => main());
